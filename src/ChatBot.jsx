db369b5919b74b8c5b49c0320e39ae04e412cf11
Vikas Namdev <itsvikas2974@gmail.com>
2025-03-06 12:02:44 +0530

    Update ChatBot.jsx

--- src/ChatBot.jsx
+++ src/ChatBot.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import "./Chatbot.css";
+import "./ChatBot.css";
 import { FaPaperPlane } from "react-icons/fa";
 
 const ChatBot = () => {
